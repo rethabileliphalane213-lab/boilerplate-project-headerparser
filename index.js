d707a92ebf62842afca2b5fec67bf89798474f95
Rethabile <rethabileliphalane213@gmail.com>
2026-05-19 08:02:45 +0000

    fix ipaddress

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/', function (req, res) {
 
 // your first API endpoint...
 app.get('/api/whoami', function (req, res) {
-  res.json({ ipAddress: req.socket.remoteAddress,
+  res.json({ ipaddress: req.socket.remoteAddress,
     language: req.headers['accept-language'],
     software:req.headers['user-agent']
    });
